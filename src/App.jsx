df4942fd8889743658672fae7cc16f2f801d367e
Ttuucaa <tomaslattuca@gmail.com>
2025-09-07 22:31:29 -0300

    update
    instalacion de boostrap y router, creacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Dashboard from './components/dashboard/dashboard';
+import Login from './components/auth/login';
+import Profile from './components/profile/Profile';
+import Routines from './components/routines/Routines';
+import NotFound from './components/ui/NotFound';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-   <div>
-    
-      <h2>"Books Champion App"</h2>
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/routines" element={<Routines />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<NotFound />} /> // lleva a NotFound si no coincide con ninguna ruta
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
-//primero abrir la carpeta:PROYECTO-PROGRAMACIO3,luego *npm run dev-para cmd//
+export default App;
---
+++ src/components/routines/Routines.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Routines = [
+    {
+    id: 1,
+    name: "Full body",
+    days: "lunes, miercoles y viernes",
+    description: "Rutina para trabajar todo el cuerpo en una sola sesión de musculacion."
+},
+{
+    id: 2,
+    name: "Upper body",
+    days: "martes y jueves",
+    description: "Rutina para trabajar la parte superior del cuerpo en musculacion."
+},
+{
+    id: 3,
+    name: "Lower body",
+    days: "lunes, miercoles y viernes",
+    description: "Rutina para trabajar la parte inferior del cuerpo en musculacion."
+},
+{
+    id: 4,
+    name: "Yoga",
+    days: "martes y jueves",
+    description: "Rutina para trabajar la flexibilidad y el equilibrio."
+},
+{
+    id: 5,
+    name: "Funcional",
+    days: "lunes, miercoles y viernes",
+    description: "Rutina para trabajar la fuerza y la resistencia de manera integral."
+},
+{
+    id: 6,
+    name: "Pilates",
+    days: "martes y jueves",
+    description: "Rutina para trabajar la fuerza y la flexibilidad a través de movimientos controlados."
+}
+]
+
+export default Routines;
