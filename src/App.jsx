1fa7f0c551d2149dd158958f28e17106f6241cea
Ttuucaa <tomaslattuca@gmail.com>
2025-10-22 12:00:39 -0300

    acerca de & politys
    agregadas

--- src/App.jsx
+++ src/App.jsx
@@ -6,13 +6,14 @@ import Header from "./Header/Header.jsx";
 import Footer from "./Footer/Footer.jsx";
 import Main from "./Main/Main.jsx";
 import Dashboard from "./pages/dashboard/Dashboard.jsx";
-
+import About from './Footer/About.jsx';
 
 import Login from "./components/auth/login/Login.jsx";
 import Register from "./components/auth/register/Register.jsx";
-import Profile from "./profile/Profile.jsx";
+import Profile from "./Profile/Profile.jsx";
 import NewRoutine from "./pages/newRoutine/NewRoutine.jsx";
 import NotFound from "./components/ui/NotFound.jsx";
+import Polity from "./Footer/Polity.jsx";
 
 import UserDashboard from "./pages/roles/alumno/UserDashboard.jsx";
 import TrainerDashboard from "./pages/roles/Profesores/TrainerDashboard.jsx";
@@ -35,7 +36,9 @@ function App() {
             <Route path="/new-routine" element={<NewRoutine onRoutineAdded={(r) => console.log(r)}/>} />
             <Route path="/dashboard" element={<Dashboard />} />
             <Route path="/profile" element={<Profile />} />
+            <Route path="/about" element={<About />} />
             <Route path="*" element={<NotFound />} />
+            <Route path="/privacy" element={<Polity />} />
 
             {/* Dashboards según rol */}
            <Route path="/alumno/dashboard" element={<UserDashboard />} />
